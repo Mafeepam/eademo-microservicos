994485324125ca4d6b42bdedb4b68c46422eabdb
Ana Júlia Gusmão <129332966+anajugn@users.noreply.github.com>
2025-06-05 19:03:33 -0300

    Update ReservaController.java

--- reservas-service/src/main/java/com/espacos_academicos/reservasservice/controller/ReservaController.java
+++ reservas-service/src/main/java/com/espacos_academicos/reservasservice/controller/ReservaController.java
@@ -14,46 +14,47 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/reservas")
+@RestController //pronto para receber requisições HTTP e retornar respostas
+@RequestMapping("/api/reservas") //define os endpoints 
 public class ReservaController {
 
     @Autowired
     private ReservaService reservaService;
 
-    @PostMapping
+    @PostMapping //criar novas reservas pelo reservarequest, valida com o valid, retorna 201 Created
     public ResponseEntity<ReservaResponseDTO> criarReserva(@Valid @RequestBody ReservaRequestDTO reservaRequestDTO) {
         ReservaResponseDTO novaReserva = reservaService.criarReserva(reservaRequestDTO);
         return new ResponseEntity<>(novaReserva, HttpStatus.CREATED);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/{id}")//buscar reserva por id
     public ResponseEntity<ReservaResponseDTO> buscarReservaPorId(@PathVariable(name = "id") Long id) {
         ReservaResponseDTO reserva = reservaService.buscarReservaPorId(id);
         return ResponseEntity.ok(reserva);
     }
 
-    @GetMapping
+    @GetMapping //listar todas as reservas
     public ResponseEntity<List<ReservaResponseDTO>> listarTodasReservas() {
         List<ReservaResponseDTO> reservas = reservaService.listarTodasReservas();
         return ResponseEntity.ok(reservas);
     }
 
-    @GetMapping("/por-professor/{professorId}")
+    @GetMapping("/por-professor/{professorId}")//listar reservas por professor
     public ResponseEntity<List<ReservaResponseDTO>> listarReservasPorProfessor(@PathVariable Integer professorId) {
         List<ReservaResponseDTO> reservas = reservaService.listarReservasPorProfessor(professorId);
         return ResponseEntity.ok(reservas);
     }
 
-    @GetMapping("/por-espaco/{espacoId}")
+    @GetMapping("/por-espaco/{espacoId}") //listar reservas por espaço
     public ResponseEntity<List<ReservaResponseDTO>> listarReservasPorEspacoEData(
             @PathVariable Long espacoId,
+            //resquest e datetimeformar pra converter data
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate data) {
         List<ReservaResponseDTO> reservas = reservaService.listarReservasPorEspacoEData(espacoId, data);
         return ResponseEntity.ok(reservas);
     }
 
-    @PatchMapping("/{id}/status")
+    @PatchMapping("/{id}/status") //atualizar o status pelo reservaStatusUpdateRequestDTO
     public ResponseEntity<ReservaResponseDTO> atualizarStatusReserva(
             @PathVariable Long id,
             @Valid @RequestBody ReservaStatusUpdateRequestDTO statusUpdateRequest) {
